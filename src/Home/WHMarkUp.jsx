b6f7df6d56195a957438bd553ea8e294fab2fd87
Ricardo <rr155914@gmail.com>
2024-09-17 17:54:38 -0600

    add physics

--- src/Home/WHMarkUp.jsx
+++ src/Home/WHMarkUp.jsx
@@ -2,21 +2,19 @@ import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import Warehouse from './Warehouse'
 import { OrbitControls } from '@react-three/drei'
-
-
+import { Physics } from '@react-three/cannon'
 
 const WHMarkUp = ({height, width}) => {
-
   return (
    <div>
     <Canvas style={{ height, width }}>
         <OrbitControls/>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
-      <Warehouse position={[1,-2,-2]} rotation={[Math.PI / 1.8, Math.PI / -2, Math.PI / 2.2 ]} />
+      <Physics>
+        <Warehouse scale={.1} position={[,-2,-2]} rotation={[Math.PI / 2.4, Math.PI / -2, Math.PI / 2.2 ]} />
+      </Physics>
     </Canvas>
- 
-
     </div>
   )
 }
